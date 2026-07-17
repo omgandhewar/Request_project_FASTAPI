d3c4523033e68496e66bfbf8812922e967ba0f61
omgandhewar <gandhewarom@gmail.com>
2026-07-17 20:15:53 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/query")
+def query(name:str,age:int):
+    return{
+        "name":name,
+        "age":age
+    }
